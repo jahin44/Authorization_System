ff1746b4b1e52fdd0d85b12611c7e742032ceae9
Jahin Hasan <jahinhasan1@gmail.com>
2022-09-28 02:24:18 +0600

    User Operation

---
+++ Authorization_System/Authorization_System.API/Controllers/UserOperation.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Authorization_System.API.Controllers
+{
+    public class UserOperation : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Authorization_System/Authorization_System.API/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Authorization_System.API.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsAdmin { get; set; }
+
+    }
+}
